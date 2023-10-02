6635bd5d7fd76d4d29dea676d40bffc0a1a8f4e1
Nahuel Bogado <ultrashunior@gmail.com>
2023-10-02 16:44:00 -0300

    dynamic profile photo

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,14 +7,16 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
-import imgPerfilWhite from "@/public/img_pefil.jpg"
-import imgPerfilDark from "@/public/img_pefil.jpg"
+import imgPerfilWhite from "@/public/img_perfil_light.jpg";
+import imgPerfilDark from "@/public/img_pefil_dark.jpg";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import { useTheme } from "@/context/theme-context";
 
 export default function Intro() {
   const { ref } = useSectionInView("Inicio", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const { theme } = useTheme();
 
   return (
     <section
@@ -32,27 +34,28 @@ export default function Intro() {
               duration: 0.2,
             }}
           >
-            <Image
-              src={imgPerfilWhite}
-              alt="Imagen de Nahuel"
-              width="192"
-              height="192"
-              quality="100"
-              priority={true}
-              className="h-26 w-26 rounded-full object-cover border-[0.3rem] border-fuchsia-200 dark:border-slate-600 shadow-xl dark:hidden sm:max-md:h-22 sm:max-md:w-22"
-            />
-            <Image
-              src={imgPerfilDark}
-              alt="Fotografía de Nahuel"
-              width="192"
-              height="192"
-              quality="100"
-              priority={true}
-              className="h-26 w-26 rounded-full object-cover border-[0.3rem] border-fuchsia-200 dark:border-slate-600 shadow-xl light:hidden sm:max-md:h-22 sm:max-md:w-22"
-            />
+            {theme === "light" ? (
+              <Image
+                src={imgPerfilWhite}
+                alt="Imagen de Nahuel"
+                width="192"
+                height="192"
+                quality="100"
+                priority={true}
+                className="h-26 w-26 rounded-full object-cover border-[0.3rem] border-pink-200 shadow-xl dark:hidden sm:max-md:h-22 sm:max-md:w-22"
+              />
+            ) : (
+              <Image
+                src={imgPerfilDark}
+                alt="Fotografía de Nahuel"
+                width="192"
+                height="192"
+                quality="100"
+                priority={true}
+                className="h-26 w-26 rounded-full object-cover border-[0.3rem] dark:border-slate-600 shadow-xl sm:max-md:h-22 sm:max-md:w-22"
+              />
+            )}
           </motion.div>
-
-          
         </div>
       </div>
 
@@ -61,9 +64,16 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <p>Hola, me llamo <span className="font-bold">Nahuel.</span>  </p>
-        <p>Soy <span className="font-bold"> desarrollador web FullStack</span></p>
-        <p> y <span className="font-bold"> profesor de programación.</span></p>
+        <p>
+          Hola, me llamo <span className="font-bold">Nahuel.</span>{" "}
+        </p>
+        <p>
+          Soy desarrollador web <span className="font-bold"> FullStack</span>
+        </p>
+        <p>
+          {" "}
+          y <span className="font-bold"> profesor </span> de programación.
+        </p>
         <p className="pt-[14rem] text-2xl">Para información acá 👇</p>
       </motion.h1>
 
@@ -75,27 +85,6 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-        <Link
-          href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {
-            setActiveSection("Contacto");
-            setTimeOfLastClick(Date.now());
-          }}
-        >
-          Contactame{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
-
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV Nahuel Bogado.pdf"
-          download
-        >
-          Descargar CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
-
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/ultrashunior/"
@@ -111,6 +100,26 @@ export default function Intro() {
         >
           <FaGithubSquare />
         </a>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/CV Nahuel Bogado.pdf"
+          target="_blank"
+          download
+        >
+          CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contacto");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contactame{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
       </motion.div>
     </section>
   );
