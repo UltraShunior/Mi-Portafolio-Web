12722e4940a72ee2a28197a987b2132ff9ed54a9
NaweDev <ultrashunior@gmail.com>
2024-06-26 01:09:19 -0300

    update proyects views

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,9 +10,9 @@ export default function Projects() {
   const { ref } = useSectionInView("Proyectos", 0.5);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28 w-[80%]">
       <SectionHeading>Mis proyectos 📚</SectionHeading>
-      <div>
+      <div className="grid grid-cols-3 grid-rows-2 gap-8" >
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
