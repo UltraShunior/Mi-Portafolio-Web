cfc8623de92e2218257ec598763b19047412b708
ultrashunior <ultrashunior@gmail.com>
2024-07-04 18:09:56 +0000

    light-mode: background color changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,11 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#f6cdd6] absolute top-[24rem] right-[14rem] -z-5 h-[31rem] w-[44rem] rounded-full blur-[12rem] sm:w-[44rem] dark:bg-[#cd8486d1]/70"></div>
-        <div className="bg-[#f6cdd6] absolute top-[24rem] right-[16rem] -z-5 h-[31rem] w-[50rem] rounded-full blur-[10rem] sm:w-[50rem] dark:bg-[#676394]"></div>
-        <div className="bg-[#a9d7e9] absolute top-[24rem] left-[-38rem] -z-5 h-[31rem] w-[44rem] rounded-full blur-[12rem] sm:w-[48rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-10rem] 2xl:left-[8rem] dark:bg-[#cd8486d1]/90"></div>
-        <div className="bg-[#a9d7e9] absolute top-[24rem] left-[-38rem] -z-5 h-[31rem] w-[50rem] rounded-full blur-[10rem] sm:w-[54rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-10rem] 2xl:left-[6rem] dark:bg-[#676394]/80"></div>
+        <div className="bg-[#b700ff55] absolute top-[24rem] right-[14rem] -z-5 h-[31rem] w-[44rem] rounded-full blur-[18rem] max-sm:h-[20rem] max-sm:right-[4rem] max-sm:top-[12rem] sm:w-[44rem] sm:h-[20rem] dark:blur-[12rem] dark:bg-[#cd8486d1]/70"></div>
+        <div className="bg-[#676394] absolute top-[24rem] right-[16rem] -z-5 h-[31rem] w-[50rem] rounded-full blur-[10rem] max-sm:h-[20rem] max-sm:right-[4rem] max-sm:top-[12rem] sm:w-[50rem] hidden dark:block"></div>
+
+        <div className="bg-[#f6cdd6] absolute top-[24rem] left-[-38rem] -z-5 h-[31rem] w-[44rem] rounded-full blur-[12rem] max-sm:hidden sm:w-[48rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-10rem] 2xl:left-[8rem] dark:bg-[#cd8486d1]/90"></div>
+        <div className="bg-[#676394]/80 absolute top-[24rem] left-[-38rem] -z-5 h-[31rem] w-[50rem] rounded-full blur-[10rem] max-sm:hidden sm:w-[54rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-10rem] 2xl:left-[6rem] hidden dark:block"></div>
 
 
         <ThemeContextProvider>
@@ -36,12 +37,11 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
-
-
             <Toaster position="top-right" />
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
