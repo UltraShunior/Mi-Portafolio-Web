1f4cb44909992f1bac538eafdcc3e5404b3e53bc
ultrashunior <ultrashunior@gmail.com>
2024-07-06 00:19:09 +0000

    updated about text

--- components/about.tsx
+++ components/about.tsx
@@ -23,10 +23,10 @@ export default function About() {
         <b>desarrollo de software</b>. Comencé trabajando como <b>freelancer</b>
         , creando soluciones con HTML, CSS, PHP y JavaScript. {" "}
         En 2022, di el salto a la <b> industria tecnológica</b> al unirme a una consultora como <b>Desarrollador de Software.</b> {" "}
-        Además, tras graduarme como <b>Técnico Programador</b>, a principios de 2023 comencé a enseñar en la misma institución donde me formé, compartiendo mi pasión por la programación con los estudiantes.
+        Además, tras graduarme como <b>Técnico Programador</b>, a principios de 2023 comencé a impartir clases de 'Proyecto, diseño e implementación de Sistemas Computacionales', compartiendo mi pasión por la programación con futuros técnicos informáticos.
       </p>
       <p>
-      En la actualidad, mi especialización se enfoca en el desarrollo de interfaces de usuario dinámicas y responsivas con <b>Next.js</b> y <b>Tailwind CSS</b>  para el frontend, así como en la creación de aplicaciones de servidor robustas utilizando <b>Node.js</b> y <b>Express</b> para el backend. Tengo competencia en el manejo de bases de datos SQL, trabajando tanto con <b>PostgreSQL</b> como con <b>MySQL</b>. Continúo ampliando mis habilidades técnicas y estoy en proceso de capacitación en <b>MongoDB</b>, <b>AWS</b>, <b>Jest</b>, <b>Cypress</b>, <b>Playwright</b> y prácticas de <b>Integración y Entrega Continuas (CI/CD)</b>, lo que fortalece mi perfil como desarrollador Full Stack.
+      En la actualidad, mi especialización se enfoca en el desarrollo de aplicaciones web con <b>Next.js</b> y <b>Tailwind CSS</b> para el frontend, y utilizando <b>Node.js</b> y <b>Express</b> para el backend. Tengo competencia en el manejo de bases de datos SQL, trabajando tanto con <b>PostgreSQL</b> como con <b>MySQL</b>. Continúo ampliando mis habilidades técnicas y estoy en proceso de capacitación en <b>AWS</b>, <b>Jest</b>, <b>Cypress</b>, <b>Playwright</b> y prácticas de <b>Integración y Entrega Continuas (CI/CD)</b>, lo que fortalecerá mi perfil como desarrollador Full Stack.
       </p> 
     </motion.section>
   );
