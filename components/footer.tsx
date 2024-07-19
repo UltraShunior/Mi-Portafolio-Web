2005f29e7827a1b8602519b27867c53dbf7611d8
ultrashunior <ultrashunior@gmail.com>
2024-07-19 13:13:48 +0000

    update footer date

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        Copyright © 2023 UltraShunior.
+        Copyright © {new Date().getFullYear()} UltraShunior.
       </small>
       <p className="text-xs">
         <span className="font-semibold">Sobre este sitio:</span> construido con
